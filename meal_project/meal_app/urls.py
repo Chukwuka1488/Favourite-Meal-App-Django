c2963b1b6192b40b53ec437848ee72783b725965
Chuks Akibot <juniorakibor@yahoo.com>
2021-08-29 16:16:51 +0200

    details view functionality added

--- meal_project/meal_app/urls.py
+++ meal_project/meal_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.get_meals, name='get_meals'),
-    # path('meals/<int:id>/', views.meal_detail, name='meal_detail'),
+    path('meals/<int:id>/', views.meal_detail, name='meal_detail'),
 ]
